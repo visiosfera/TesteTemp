4d30be3528a7b9c74b2dfb426d1d4a9b0f5737a0
visiosfera <ermano@gmail.com>
2025-06-28 16:35:26 -0300

    alteração

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import node from "@astrojs/node";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: node({
-      mode: "standalone",
-    }),
+    output: "static",
 });
