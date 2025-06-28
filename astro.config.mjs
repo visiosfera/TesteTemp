5b44ccc191db9cd90478f29ff599632ada3063c2
visiosfera <ermano@gmail.com>
2025-06-28 16:43:55 -0300

    alteração

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
+    preview: {
+		port: 4321,
+		host: true,
+	},
+	server: {
+		host: true,
+		port: 4321
+	}
 });
